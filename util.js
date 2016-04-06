8dc43bdb879c79bcc5ebdf211a4ab07073f64f00
CooHunter <chenfeifan89@gmail.com>
2016-04-06 15:54:43 +0800

    Create util.js

---
+++ util.js
@@ -0,0 +1,18 @@
+var Util = {
+  IE: function() {
+    if (document.documentMode) {
+      return document.documentMode;
+    } else {
+      for (var i = 7; i > 4; i--) {
+        var div = document.createElement('div');
+        div.innerHTML = '<!--[if IE ' + i + ']><span></span><![endif]-->';
+        
+        if (div.getElementsByTagName('span').length) {
+          div = null;
+          return i;
+        }
+      }
+    }
+    return undefined;
+  }
+}
